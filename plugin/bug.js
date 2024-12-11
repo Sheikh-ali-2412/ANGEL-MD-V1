cc88ac849af99f9ef90753b2016ba7ec77242056
zeusnew <160241147+zeusnew@users.noreply.github.com>
2024-12-10 23:42:28 -0800

    Create bug.js

---
+++ plugin/bug.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+const config = require('../config');
+const { xeontext1 } = require('../Media/xeontext1'); 
+
+
+//======================================================================================================================
+cmd({
+    pattern: "bug",
+    desc: "Send a bug report message.",
+    react: "✔",
+    category: "main",
+    filename: __filename
+},
+async (conn, mek, m, { reply }) => {
+    try {
+        const senderNumber = m.sender;
+
+       
+        return reply(xeontext1);
+    } catch (e) {
+        console.log(e);
+        // Handle any errors by replying with the error message
+        return reply(`${e}`);
+    }
+});
